838e3199639fb0d2b7530de470aa76dd26ce1aa4
bonioss <slash98.pb@gmail.com>
2020-06-17 00:54:31 +0200

    test bug fix

--- server/routes/tests.routes.js
+++ server/routes/tests.routes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
 const { createTest } = require('../controllers/tests.controller');
 
-router
-    .route('/group/:id')
-    .post(protect, authorize('teacher', 'admin'), createTest);
-    // .post(createTest);
+router.route('/').post(protect, authorize('teacher', 'admin'), createTest);
+// .post(createTest);
 
 module.exports = router;
